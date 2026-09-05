d3f8314e84e666339cadff8da6a0666e8f0574df
Tung Huynh <137667812+acchtt@users.noreply.github.com>
2026-09-05 23:49:28 +0700

    Log startup crashes for YT Music Lite

--- yt-music-lite/Program.cs
+++ yt-music-lite/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using System.IO;
 using System.Net;
+using System.Text;
 using System.Windows.Forms;
 
 namespace YTMusicLite
@@ -9,13 +11,41 @@ namespace YTMusicLite
         [STAThread]
         static void Main()
         {
-            // GitHub requires TLS 1.2+. Older .NET Framework defaults can try TLS 1.0
-            // and fail with "Could not create SSL/TLS secure channel".
-            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            try
+            {
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    string dir = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                        "YTMusicLite");
+                    Directory.CreateDirectory(dir);
+                    string log = Path.Combine(dir, "startup-crash.log");
+                    File.WriteAllText(log, DateTime.Now.ToString("s") + Environment.NewLine + ex.ToString(), Encoding.UTF8);
+                }
+                catch
+                {
+                }
+
+                try
+                {
+                    MessageBox.Show(
+                        "YT Music Lite could not start. A crash log was saved to %LOCALAPPDATA%\\YTMusicLite\\startup-crash.log.\r\n\r\n" + ex.Message,
+                        "YT Music Lite",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+                catch
+                {
+                }
+            }
         }
     }
 }
