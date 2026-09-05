cc3d57f4d4e274195d21fccef7e11ce24f0ab7ac
Tung Huynh <137667812+acchtt@users.noreply.github.com>
2026-09-06 00:47:04 +0700

    Bump updater to 4.1.6

--- yt-music-lite/UpdateService.cs
+++ yt-music-lite/UpdateService.cs
@@ -13,7 +13,7 @@ namespace YTMusicLite
 {
     public sealed class UpdateService
     {
-        public const string CurrentVersion = "4.1.5";
+        public const string CurrentVersion = "4.1.6";
         public const string RepositoryOwner = "acchtt";
         public const string RepositoryName = "YT-Music-Lite";
         public const string ReleaseTagPrefix = "ytmlite-v";
