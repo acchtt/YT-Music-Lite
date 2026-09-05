0bc1f53360a394fc1a2d9279aa09fa3efdf04464
Tung Huynh <137667812+acchtt@users.noreply.github.com>
2026-09-05 23:33:01 +0700

    Force TLS 1.2 for GitHub updater

--- yt-music-lite/Program.cs
+++ yt-music-lite/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Windows.Forms;
 
 namespace YTMusicLite
@@ -8,6 +9,10 @@ namespace YTMusicLite
         [STAThread]
         static void Main()
         {
+            // GitHub requires TLS 1.2+. Older .NET Framework defaults can try TLS 1.0
+            // and fail with "Could not create SSL/TLS secure channel".
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
